f819c7bd2638f5d886bd63f48ac31bc9aabcfaff
Gagandeep Singh <whg_27@yahoo.com>
2018-01-05 01:08:40 -0500

    test

--- visuals/Sankey/Sankey/Sankey.js
+++ visuals/Sankey/Sankey/Sankey.js
@@ -20,6 +20,7 @@ Array.prototype.diff = function(a) {
 };
 
 visualizationFunctions.Sankey = function(element, data, opts) {
+  d3.json("data/disciplineColors.json", setDisciplineColors);
   var context = this
   this.config = this.CreateBaseConfig();
   this.SVGBase = this.config.easySVG(element[0])
@@ -425,7 +426,7 @@ visualizationFunctions.Sankey = function(element, data, opts) {
 
   function createNodes() {
 
-    d3.json("data/disciplineColors.json", setDisciplineColors);
+
     context.SVG.nodes = context.SVG.group.append("g").selectAll(".node")
     .data(graph.nodes)
     .enter().append("g")
