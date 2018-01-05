fa48a1728f0e4f0d5f0bb4c686940bbce8857ef0
Gagandeep Singh <whg_27@yahoo.com>
2018-01-05 00:04:13 -0500

    several (for test) - see description
    deleting unnecessary files + adding disciplineColors.json

--- src/DatasourceMap.js
+++ src/DatasourceMap.js
@@ -1,28 +1,9 @@
-/** @global 
- * @description Maps pretty names for datasource URLs. Mapped from (ng-data-field) 
+/** @global
+ * @description Maps pretty names for datasource URLs. Mapped from (ng-data-field)
  * @type {Object} */
  var globalDatasourceMap = {
-    one: {
-        url: '../../data/sampleData.json',
-        type: 'static'
-    },
-    sampleGeoData: {
-        url: '../../data/sampleGeoData.json',
-        type: 'static'
-    },
-    apiTest: {
-        url: 'http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&format=json',
-        type: 'api',
-        params: {
-            api_key: '',
-            artist: 'QUERYSTRING'
-        }
-    },
     copiData: {
         url: 'data/copi2.json'
-    },
-    copiForBG: {
-        url: 'data/copiDataAsRecords.json'
     },
     sankeyData: {
         url: 'data/sankey9.json'
